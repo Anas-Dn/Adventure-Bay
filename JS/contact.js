13fbb1057add621663009b91e4eb8a66b478f4e9
Anas Diouane <zeroanas18@gmail.com>
2026-04-22 11:46:31 +0100

    finished contact section

---
+++ JS/contact.js
@@ -0,0 +1,28 @@
+ lucide.createIcons();
+
+
+// Arrow Top 
+const arrowTop = () => {
+    if(window.scrollY > 30 ){
+        document.getElementById("top").style.display = "block";
+    }else{
+        document.getElementById("top").style.display = "none";
+    }
+}
+window.addEventListener("scroll", arrowTop);
+document.getElementById("top").addEventListener("click" ,() => {
+  window.scrollTo({top:0, behavior:"smooth"});
+})
+
+
+
+//header scroll
+const headerF = () => {
+  if(window.scrollY > 15 ){
+    document.getElementById("header").classList.add('scrolled-header')
+  }else{
+    document.getElementById("header").classList.remove('scrolled-header')
+  }
+}
+window.addEventListener("scroll", headerF);
+
